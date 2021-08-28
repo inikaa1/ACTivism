ea874b91f9ee6894bcaca0fb141ec7fd5b566927
RyanPadamadan <89624595+RyanPadamadan@users.noreply.github.com>
2021-08-28 16:11:30 +0530

    Add files via upload

--- activism_main/posts.php
+++ activism_main/posts.php
@@ -2,11 +2,12 @@
 include "dbconnect.php";
 $topic = $_POST['topic'];
 $place = $_POST['place'];
+$date = $_POST['date_and_time'];
 $caption = $_POST['caption'];
 $perpetrators = $_POST['perpetrators'];
 
 
-$sql = "INSERT INTO post (topic, place, caption, perpetrators)
+$sql = "INSERT INTO post (topic, place, date_and_time, caption, perpetrators)
 VALUES ('$topic', '$place', '$caption', '$perpetrators')";
 
 if ($conn->query($sql) === TRUE) {
